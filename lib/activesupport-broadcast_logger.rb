28a90101499276bad7e69fa1484cdf0d07d0db2a
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 00:16:48 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/activesupport-broadcast_logger.rb
+++ lib/activesupport-broadcast_logger.rb
@@ -28,6 +28,6 @@ require_relative "activesupport/broadcast_logger/version"
 module Activesupport
 end
 
-Activesupport::BroadcastLogger::Version.class_eval do
+ActiveSupport::BroadcastLogger::Version.class_eval do
   extend VersionGem::Basic
 end
--- lib/activesupport/broadcast_logger/version.rb
+++ lib/activesupport/broadcast_logger/version.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-module Activesupport
+module ActiveSupport
   module BroadcastLogger
+    # Version namespace for this gem.
     module Version
+      # Current gem version.
       VERSION = "2.0.4"
     end
+    # Current gem version exposed at the traditional constant location.
+    VERSION = Version::VERSION # Traditional Constant Location
   end
 end
