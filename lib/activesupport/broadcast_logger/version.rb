1acf56d60bc5a4eddd0b17d35db8df1afb363098
Peter H. Boling <peter.boling@gmail.com>
2026-08-31 19:48:10 -0400

    🎨 Template bootstrap by kettle-jem v7.1.13

--- lib/activesupport/broadcast_logger/version.rb
+++ lib/activesupport/broadcast_logger/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ActiveSupport
-  class BroadcastLogger
+  module BroadcastLogger
     # Version namespace for this gem.
     module Version
       # Current gem version.
